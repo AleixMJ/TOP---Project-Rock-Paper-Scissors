96049cf8eec450df8d2e4cc820f1bfe10d7b2c65
AleixMJ <aleixtechIT@gmail.com>
2026-01-29 12:39:52 +0100

    Playround created and tested
    The code now has a new function Playround that plays the Human Selection
    against the Computer selection and determines a winner. Code was tested
    and debugged.

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -2,6 +2,12 @@
 
 const prompt = require('prompt-sync')();
 
+let humanScore = 0;
+let computerScore = 0;
+
+
+
+
 // This functions randomly returns: Paper, Scissors or Rock // 
 
 function getComputerChoice() {
@@ -21,5 +27,43 @@ function getHumanChoice() {
     return humanAnswer;
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+function playRound(humanChoice,computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+
+    if (humanChoice == computerChoice ) {
+        return "It's a tie, play again!"
+    }
+    else if (humanChoice == "paper" && computerChoice == "rock") {
+        humanScore++;
+        return "Human wins!"
+    }
+        else if (humanChoice == "paper" && computerChoice == "scissors") {
+        computerScore++;
+        return "Computer wins!"
+    }
+        else if (humanChoice == "scissors" && computerChoice == "paper") {
+        humanScore++;
+        return "Human wins!"
+    }
+        else if (humanChoice == "scissors" && computerChoice == "rock") {
+        computerScore++;
+        return "Computer wins!"
+    }
+            else if (humanChoice == "rock" && computerChoice == "scissors") {
+        humanScore++;
+        return "Human wins!"
+    }
+        else if (humanChoice == "rock" && computerChoice == "paper") {
+        computerScore++;
+        return "Computer wins!"
+    }
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(humanSelection);
+console.log(computerSelection);
+console.log(playRound(humanSelection,computerSelection));
