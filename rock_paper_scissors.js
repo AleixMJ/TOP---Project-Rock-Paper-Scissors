f693b1271bdc6299dfcd15f59d083a8491be64a3
AleixMJ <aleixtechIT@gmail.com>
2026-01-29 12:03:17 +0100

    Function getComputerChoice created
    This functions returns randomly; Rock, Scissors or Paper. Math.random()
    was used with three different conditions. The function was tested and
    works correctly

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+// This functions randomly returns: Paper, Scissors or Rock // 
+
+function getComputerChoice() {
+    let random = Math.random() * 100;
+    if (random < 33) {
+        return "Paper";
+    }
+    else if (random > 66) {
+        return "Scissors";
+    }
+    else return "Rock";
+
+}
+
+
+console.log(getComputerChoice());
